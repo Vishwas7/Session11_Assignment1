2bc7d8ad42b2dc5782137bda1ecb3c5be79e4bff
Vishwas7 <svishwas788@gmail.com>
2017-07-03 14:18:11 +0530

    Session11_Assignment1

---
+++ src/Assignment1/ExceptionHandling.java
@@ -0,0 +1,17 @@
+package Assignment1;
+
+/*
+ * In this program I've written a code in Java to declare an integer array of 5 elements and try to 
+ * print the value of the 6th element.
+ * Which will show in the console.
+ */
+
+public class ExceptionHandling { // class of this program
+
+	public static void main(String[] args) { // main function
+		
+		int[]myarray=  new int[]{1,2,3,4,5,}; 
+		System.out.println(myarray[5]);
+	}
+ 
+}
